1970a214d2ebf97e62abdf110b2f54e872ca6a49
PinkGoosik <MCGycuk@yandex.ru>
2023-03-19 00:48:05 +0500

    update to 1.19.4

--- src/main/java/ru/tpsd/eatinganimationmod/mixin/ItemRendererMixin.java
+++ src/main/java/ru/tpsd/eatinganimationmod/mixin/ItemRendererMixin.java
@@ -1,20 +1,19 @@
 package ru.tpsd.eatinganimationmod.mixin;
 
-
 import net.minecraft.client.render.item.ItemModels;
 import net.minecraft.client.render.item.ItemRenderer;
 import net.minecraft.client.render.model.BakedModel;
-import net.minecraft.client.world.ClientWorld;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.resource.SynchronousResourceReloader;
 import net.minecraft.world.World;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ItemRenderer.class)
 public abstract class ItemRendererMixin implements SynchronousResourceReloader {
@@ -22,25 +21,19 @@ public abstract class ItemRendererMixin implements SynchronousResourceReloader {
 	@Final
 	private ItemModels models;
 
-	@Shadow public abstract BakedModel getModel(ItemStack stack, @Nullable World world, @Nullable LivingEntity entity, int seed);
+	@Shadow
+	protected abstract void renderGuiItemModel(MatrixStack matrices, ItemStack stack, int x, int y, BakedModel model);
 
-	public BakedModel getHeldItemModel_1(ItemStack stack, @Nullable World world, @Nullable LivingEntity entity, int seed) {
+	@Inject(method = "innerRenderInGui(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/entity/LivingEntity;Lnet/minecraft/world/World;Lnet/minecraft/item/ItemStack;IIII)V", at = @At("HEAD"), cancellable = true)
+	void innerRenderInGui(MatrixStack matrices, LivingEntity entity, World world, ItemStack stack, int x, int y, int seed, int depth, CallbackInfo ci) {
+		if (!stack.isEmpty() && stack.isFood()) {
+			BakedModel bakedModel = this.models.getModel(stack);
+			matrices.push();
+			matrices.translate(0.0F, 0.0F, (float) (50 + (bakedModel.hasDepth() ? depth : 0)));
+			this.renderGuiItemModel(matrices, stack, x, y, bakedModel);
 
-		BakedModel bakedModel3 = this.models.getModel(stack);
-
-		ClientWorld clientWorld = world instanceof ClientWorld ? (ClientWorld)world : null;
-		BakedModel bakedModel4 = stack.isFood()
-			? this.models.getModel(stack)
-			: bakedModel3.getOverrides().apply(bakedModel3, stack, clientWorld, entity, seed);
-
-		return bakedModel4 == null ? this.models.getModelManager().getMissingModel() : bakedModel4;
-	}
-
-	@ModifyVariable(method = "innerRenderInGui(Lnet/minecraft/entity/LivingEntity;Lnet/minecraft/item/ItemStack;IIII)V", at = @At("STORE"), ordinal = 0)
-	private BakedModel injected(BakedModel bakedModel, @Nullable LivingEntity entity, ItemStack itemStack, int seed) {
-		if(itemStack.isFood()){
-			return this.getHeldItemModel_1(itemStack, null, entity, seed);
+			matrices.pop();
+			ci.cancel();
 		}
-		return getModel(itemStack, null, entity, seed);
 	}
 }
