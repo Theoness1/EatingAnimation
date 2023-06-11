e6e4b7121ed76f9883c28ad580564f105299b72f
YoungSoulluoS <70120313+YoungSoulluoS@users.noreply.github.com>
2023-06-11 08:30:19 +0500

    Bottle without cork + CustomModelData Fix

--- src/main/java/ru/tpsd/eatinganimationmod/mixin/DrawContextMixin.java
+++ src/main/java/ru/tpsd/eatinganimationmod/mixin/DrawContextMixin.java
@@ -23,12 +23,12 @@ public abstract class DrawContextMixin {
 	@ModifyVariable(method = "drawItem(Lnet/minecraft/entity/LivingEntity;Lnet/minecraft/world/World;Lnet/minecraft/item/ItemStack;IIII)V", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/client/render/item/ItemRenderer;getModel(Lnet/minecraft/item/ItemStack;Lnet/minecraft/world/World;Lnet/minecraft/entity/LivingEntity;I)Lnet/minecraft/client/render/model/BakedModel;"))
 	private BakedModel getModel(BakedModel bakedModel, @Nullable LivingEntity entity, @Nullable World world, ItemStack stack, int x, int y, int seed, int z) {
 		if(stack.isFood()){
-			return this.getHeldItemModel(stack, null, entity, seed);
+			//return this.getHeldItemModel(stack, null, entity, seed);
 		}
 		return this.client.getItemRenderer().getModel(stack, world, entity, seed);
 	}
 
-	public BakedModel getHeldItemModel(ItemStack stack, @Nullable World world, @Nullable LivingEntity entity, int seed) {
+	/*public BakedModel getHeldItemModel(ItemStack stack, @Nullable World world, @Nullable LivingEntity entity, int seed) {
 
 		BakedModel bakedModel = client.getItemRenderer().getModels().getModel(stack);
 
@@ -38,5 +38,5 @@ public abstract class DrawContextMixin {
 				: bakedModel.getOverrides().apply(bakedModel, stack, clientWorld, entity, seed);
 
 		return bakedModel1 == null ? client.getItemRenderer().getModels().getModelManager().getMissingModel() : bakedModel1;
-	}
+	}*/
 }
