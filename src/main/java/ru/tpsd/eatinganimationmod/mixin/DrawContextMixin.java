d35494b8fa0a99722c5f1ec1ddb5270a5060fee5
Theoness1 <sasha.bolotskih2002@gmail.com>
2023-10-29 18:58:00 +0700

    Custom model data fix

--- src/main/java/ru/tpsd/eatinganimationmod/mixin/DrawContextMixin.java
+++ src/main/java/ru/tpsd/eatinganimationmod/mixin/DrawContextMixin.java
@@ -3,7 +3,6 @@ package ru.tpsd.eatinganimationmod.mixin;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.render.model.BakedModel;
-import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
@@ -22,21 +21,20 @@ public abstract class DrawContextMixin {
 	@SuppressWarnings(value = "all")
 	@ModifyVariable(method = "drawItem(Lnet/minecraft/entity/LivingEntity;Lnet/minecraft/world/World;Lnet/minecraft/item/ItemStack;IIII)V", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/client/render/item/ItemRenderer;getModel(Lnet/minecraft/item/ItemStack;Lnet/minecraft/world/World;Lnet/minecraft/entity/LivingEntity;I)Lnet/minecraft/client/render/model/BakedModel;"))
 	private BakedModel getModel(BakedModel bakedModel, @Nullable LivingEntity entity, @Nullable World world, ItemStack stack, int x, int y, int seed, int z) {
-		if(stack.isFood()){
+		/*if(stack.isFood()){
 			return this.getHeldItemModel(stack, null, entity, seed);
-		}
+		}*/
 		return this.client.getItemRenderer().getModel(stack, world, entity, seed);
 	}
 
+	/*@Unique
 	public BakedModel getHeldItemModel(ItemStack stack, @Nullable World world, @Nullable LivingEntity entity, int seed) {
 
 		BakedModel bakedModel = client.getItemRenderer().getModels().getModel(stack);
 
 		ClientWorld clientWorld = world instanceof ClientWorld ? (ClientWorld)world : null;
-		BakedModel bakedModel1 = stack.isFood()
-				? client.getItemRenderer().getModels().getModel(stack)
-				: bakedModel.getOverrides().apply(bakedModel, stack, clientWorld, entity, seed);
+		BakedModel bakedModel1 = stack.isFood() ? client.getItemRenderer().getModels().getModel(stack) : bakedModel.getOverrides().apply(bakedModel, stack, clientWorld, entity, seed);
 
 		return bakedModel1 == null ? client.getItemRenderer().getModels().getModelManager().getMissingModel() : bakedModel1;
-	}
+	}*/
 }
